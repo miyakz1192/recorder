94d41dc07c7599e79997f0748657f0064feaa3f9
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2023-11-28 14:50:49 +0000

    recoder

--- recorder.py
+++ recorder.py
@@ -3,48 +3,57 @@
 
 import pika
 import ssl
-import os 
+import os
+import sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
 CERTFILE = "./localhost.pem"
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(CERTFILE)
 
+#settting of import messaging
+sys.path.append("messaging")
+from messaging import *
+
+# def queue_message(data):
+#     ip = os.environ["LLM_SVC_QUEUE_SERVER_IP"]
+#     port = os.environ["LLM_SVC_QUEUE_SERVER_PORT"]
+#     path = os.environ["LLM_SVC_QUEUE_SERVER_PATH"]
+#     user = os.environ["LLM_SVC_QUEUE_SERVER_USER"]
+#     passwd = os.environ["LLM_SVC_QUEUE_SERVER_PASSWD"]
+#     credentials = pika.PlainCredentials(user, passwd)
+#     connection = pika.BlockingConnection(
+#         pika.ConnectionParameters(ip, port, path, credentials)
+#     )
+#     channel = connection.channel()
+#     channel.queue_declare(queue='hello')
+#     channel.basic_publish(exchange='', routing_key='hello', body=data)
+#     connection.close()
+
 
 def queue_message(data):
-    ip = os.environ["LLM_SVC_QUEUE_SERVER_IP"]
-    port = os.environ["LLM_SVC_QUEUE_SERVER_PORT"]
-    path = os.environ["LLM_SVC_QUEUE_SERVER_PATH"]
-    user = os.environ["LLM_SVC_QUEUE_SERVER_USER"]
-    passwd = os.environ["LLM_SVC_QUEUE_SERVER_PASSWD"]
-    credentials = pika.PlainCredentials(user, passwd)
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(ip, port, path, credentials)
-    )
-    channel = connection.channel()
-    channel.queue_declare(queue='hello')
-    channel.basic_publish(exchange='', routing_key='hello', body=data)
-    connection.close()
+    RecoderServiceMessaging().connect_and_basic_publish(data)
+
 
 class MyHandler(CGIHTTPRequestHandler):
     def do_PUT(self):
         try:
             # PUTリクエストの対象のパスを取得
             path = "./" + self.path
-        
+
             # PUTデータのサイズを取得
             content_length = int(self.headers['Content-Length'])
 
             print("受信データを読み込みます. content_length=", content_length)
-        
-        
+
             # ここでパスとデータを処理できます
             # 例: パスに基づいてファイルにデータを書き込む
             chunks = bytes()
             with open(path, 'wb') as f:
                 # データを少しずつ読み取り、ファイルに書き込む
                 while content_length > 0:
-                    chunk = self.rfile.read(min(1024, content_length))  # 最大1024バイトずつ読み取る
+                    # 最大1024バイトずつ読み取る
+                    chunk = self.rfile.read(min(1024, content_length))
                     if not chunk:
                         print("not chunk")
                         break
@@ -54,7 +63,6 @@ class MyHandler(CGIHTTPRequestHandler):
                     chunks += chunk
                     content_length -= len(chunk)
 
-        
             # 応答を返す
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -64,16 +72,15 @@ class MyHandler(CGIHTTPRequestHandler):
         except BrokenPipeError:
             print("クライアントが接続を閉じました")
 
-
     def do_OPTIONS(self):
-       self.send_response(204)
-       self.send_header('Access-Control-Allow-Origin', '*'); # すべてのオリジンからのアクセスを許可
-       #self.send_header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE'); # 許可するメソッド
-       self.send_header('Access-Control-Allow-Methods', '*'); # 許可するメソッド
-       #self.send_header('Access-Control-Allow-Headers', 'Content-Type'); # 許可するヘッダー
-       self.send_header('Access-Control-Allow-Headers', '*'); # 許可するヘッダー
-       self.end_headers()
-	 
+        self.send_response(204)
+        # すべてのオリジンからのアクセスを許可
+        self.send_header('Access-Control-Allow-Origin', '*')
+        # 許可するメソッド
+        self.send_header('Access-Control-Allow-Methods', '*')
+        # 許可するヘッダー
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.end_headers()
 
 
 if __name__ == '__main__':
@@ -82,6 +89,3 @@ if __name__ == '__main__':
     print('サーバーがポート8001で実行中...')
     httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
     httpd.serve_forever()
-
-
-
